a879313f399fbfdb5918d8638d39fe10752b4346
Natalia Kowalik <natalia.kowalik01@gmail.com>
2021-10-20 15:12:13 +0200

    zad.5 srednia

---
+++ lab_3_zad_5/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+
+void srednia(double *x, double *y)
+{
+    cout<<"srednia = "<<((*x)+(*y))/2<<endl;
+}
+
+int main()
+{
+    double a, b;
+    cout<<"Podaj dwie liczby: ";
+    cin>>a>>b;
+    srednia(&a, &b);
+    return 0;
+}
